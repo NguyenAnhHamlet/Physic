0a1b3a3411b3086acfe55120eac5e97ca948bb49
Anh <you@example.com>
2023-12-14 21:57:30 +0700

    Define function for collisionHDL

--- src/collision_hdl.cpp
+++ src/collision_hdl.cpp
@@ -75,8 +75,8 @@ COLLISION_HDL::isCollide(RECTANGLE* rect_1, RECTANGLE* rect_2)
                 circle->getCenter().y  circle->getR().y /2 < points[3])
             return true;
         
-        return false;
     }
+    return false;
 }
 
 void
@@ -108,7 +108,22 @@ COLLISION_HDL::collisionHDL(CIRCLE* circle, RECTANGLE* rect)
 {
     if(isCollide(circle,rect))
     {
+        float m1 = circle->getMass();
+        float m2 = rect->getMass();
 
+        VECTOR v1 = circle->getVelocity();
+        VECTOR v2 = rect->getVelocity();
+
+        VECTOR c1 = circle->getCenter();
+        VECTOR c2 = rect->getCenter();
+
+        VECTOR v_fn1 = v1 - (2*m2 / (m1+m2)) 
+            *( (v1 -v2).scalarProduct(c1 - c2)/pow((c1-c2).magnitude(),2))*(c1-c2);
+        VECTOR v_fn2 = v2 - (2*m1 / (m1+m2)) 
+            *( (v2 -v1).scalarProduct(c2 - c1)/pow((c2-c1).magnitude(),2))*(c2-c1);
+
+        circle->setVelocity(v_fn1);
+        rect->setVelocity(v_fn2);
     }
 }
 
@@ -117,7 +132,22 @@ COLLISION_HDL::collisionHDL(CIRCLE* circle_1, CIRCLE* circle_2)
 {
     if(isCollide(circle_1,circle_2))
     {
-        
+        float m1 = circle_1->getMass();
+        float m2 = circle_2->getMass();
+
+        VECTOR v1 = circle_1->getVelocity();
+        VECTOR v2 = circle_2->getVelocity();
+
+        VECTOR c1 = circle_1->getCenter();
+        VECTOR c2 = circle_2->getCenter();
+
+        VECTOR v_fn1 = v1 - (2*m2 / (m1+m2)) 
+            *( (v1 -v2).scalarProduct(c1 - c2)/pow((c1-c2).magnitude(),2))*(c1-c2);
+        VECTOR v_fn2 = v2 - (2*m1 / (m1+m2)) 
+            *( (v2 -v1).scalarProduct(c2 - c1)/pow((c2-c1).magnitude(),2))*(c2-c1);
+
+        circle_1->setVelocity(v_fn1);
+        circle_2->setVelocity(v_fn2);
     }
 }
 
@@ -126,6 +156,21 @@ COLLISION_HDL::collisionHDL(RECTANGLE* rect_1, RECTANGLE* rect_2)
 {
     if(isCollide(rect_1,rect_2))
     {
-        
-    }    
+        float m1 = rect_1->getMass();
+        float m2 = rect_2->getMass();
+
+        VECTOR v1 = rect_1->getVelocity();
+        VECTOR v2 = rect_2->getVelocity();
+
+        VECTOR c1 = rect_1->getCenter();
+        VECTOR c2 = rect_2->getCenter();
+
+        VECTOR v_fn1 = v1 - (2*m2 / (m1+m2)) 
+            *( (v1 -v2).scalarProduct(c1 - c2)/pow((c1-c2).magnitude(),2))*(c1-c2);
+        VECTOR v_fn2 = v2 - (2*m1 / (m1+m2)) 
+            *( (v2 -v1).scalarProduct(c2 - c1)/pow((c2-c1).magnitude(),2))*(c2-c1);
+
+        rect_1->setVelocity(v_fn1);
+        rect_2->setVelocity(v_fn2);
+    }
 }
