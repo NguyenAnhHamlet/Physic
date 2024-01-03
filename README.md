e1c8d9e15db5043a61b794bfabfe307815a2a3c9
Anh <you@example.com>
2024-01-03 21:51:25 +0700

    get the pos for splitting the array

--- README.md
+++
@@ -1,4 +0,0 @@
-Each shape has an array which is of the size of total number of shape
-1 : checked
-0 : yet to check 
--1 : non exist
--- include/BVH.hpp
+++ include/BVH.hpp
@@ -17,6 +17,8 @@ typedef struct BVHNode
 
 } BVHNode;
 
+typedef std::pair<float<std::pair<float,float>> cost_infos;
+
 typedef std::vector<BVHNode*> BVHNodeArray;
 
 // create a BVH Node from a bound
@@ -31,17 +33,17 @@ BVHNodeArray sortBVHNodeArrY(BVHNodeArray arr);
 BVHNode* SAH(const BVHNodeArray& arr, unsigned int maxRetry = 3,
             const Bounds2D& ttBound, float Tt, float Ti, BVHNode* root);
 
-float getCost(const BVHNodeArray& arr, const Bounds2D& ttBound, 
+cost_infos getCost(const BVHNodeArray& arr, const Bounds2D& ttBound, 
                 unsigned int pos, float Tt, float Ti, axis Axis);
 
-std::pair<float, float> getMinCostXAxis(const BVHNodeArray& arr, const Bounds2D& ttBound,
+cost_infos getMinCostXAxis(const BVHNodeArray& arr, const Bounds2D& ttBound,
                                         float Tt, float Ti);
 
-std::pair<float, float> getMinCostYAxis(const BVHNodeArray& arr, const Bounds2D& ttBound,
+cost_infos getMinCostYAxis(const BVHNodeArray& arr, const Bounds2D& ttBound,
                                         float Tt, float Ti);
 
-std::pair<float, float> minCost(std::pair<float, float> cost_1, 
-                                std::pair<float, float> cost_2 );
+cost_infos minCost(cost_infos cost_1, 
+                                cost_infos cost_2 );
 
 float getArea(const Bounds2D& b);
 
